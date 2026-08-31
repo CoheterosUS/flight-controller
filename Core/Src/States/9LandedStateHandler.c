9f0f4d4c14f63a3c17e34e064b7701c92411b016
Angelo Sho Moraschi <68395930+amoraschi@users.noreply.github.com>
2026-08-31 23:49:31 +0200

    feat: implement flash logging

--- Core/Src/States/9LandedStateHandler.c
+++ Core/Src/States/9LandedStateHandler.c
@@ -2,6 +2,7 @@
 #include "Utils/Pyro.h"
 #include "Utils/Calculations.h"
 #include "Utils/SD.h"
+#include "Sensors/W25Q32JV.h"
 #include "stm32h7xx_hal.h"
 
 void LandedStateEntry(SystemContext_t *ctx) {
@@ -16,6 +17,15 @@ SystemState_t LandedStateHandler(SystemContext_t *Context, FlightData_t FlightDa
     ) {
         Context->SDLoggingEnabled = false;
         CloseFile();
+    }
+
+    if (Context->FlashLoggingEnabled
+#if LANDED_FLASH_STOP_DELAY_ENABLED
+        && GetStateElapsedMs(Context, STATE_LANDED) >= LANDED_FLASH_STOP_DELAY_MS
+#endif
+    ) {
+        Context->FlashLoggingEnabled = false;
+        W25Q_LoggingStop();
     }
 
     return STATE_LANDED;
