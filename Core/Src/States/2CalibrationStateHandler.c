a66d2c8f6597f6f4439cc30e6aaa77a9417a85e8
Angelo Sho Moraschi <68395930+amoraschi@users.noreply.github.com>
2026-09-07 18:43:27 +0200

    feat: add option to disable GPS fix

--- Core/Src/States/2CalibrationStateHandler.c
+++ Core/Src/States/2CalibrationStateHandler.c
@@ -10,9 +10,13 @@ SystemState_t CalibrationStateHandler(SystemContext_t *Context, FlightData_t Fli
 	CalibratePressure(FlightData, Context);
 	CalibrateGyroscope(FlightData, Context);
 
+#if GPS_FIX_REQUIRED
 	if (FlightData.GPSAltitude != 0.0f && FlightData.UnixTime != 0 && FlightData.Latitude != 0 && FlightData.Longitude != 0 && FlightData.Satellites >= GPS_FIX_MIN_SATELLITES) {
 		Context->GPSFixValid = true;
 	}
+#else
+	Context->GPSFixValid = true;
+#endif
 
 	if (Context->ReferencePressurePaValid && Context->GyroCalibrationValid && Context->GPSFixValid) {
 		return STATE_PRELAUNCH;
