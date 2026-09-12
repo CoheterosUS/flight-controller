e83c869f97b57166020b571bb5b3171009491e80
Angelo Sho Moraschi <68395930+amoraschi@users.noreply.github.com>
2026-09-12 20:47:39 +0200

    feat: disable data ready for imu for now

--- Core/Src/Callbacks/Callbacks.c
+++ Core/Src/Callbacks/Callbacks.c
@@ -41,13 +41,13 @@ void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart) {
 }
 
 void IIM42653_Timer_Callback(TimerHandle_t xTimer) {
-	bool Ready = false;
-	IIM42653_CheckDataReady(IIM42653_HANDLE, &Ready);
-	if (Ready) {
+	// bool Ready = false;
+	// IIM42653_CheckDataReady(IIM42653_HANDLE, &Ready);
+	// if (Ready) {
 		IIM42653_TXBuf[0] = IIM42653_REG_ACCEL_DATA_X1_UI | IIM42653_READ_MASK;
 		IIM42653_SelectCS();
 		HAL_SPI_TransmitReceive_DMA(IIM42653_HANDLE, IIM42653_TXBuf, IIM42653_RXBuf, IIM42653_SENSOR_DATA_SIZE);
-	}
+	// }
 }
 
 void BMP581_Timer_Callback(TimerHandle_t xTimer) {
