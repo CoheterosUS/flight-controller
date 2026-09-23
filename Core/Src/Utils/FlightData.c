8eca28cc3a9b973d17ba870e0218f27d0618c546
Angelo Sho Moraschi <68395930+amoraschi@users.noreply.github.com>
2026-09-23 22:32:45 +0200

    feat: start initialization of kalman

--- Core/Src/Utils/FlightData.c
+++ Core/Src/Utils/FlightData.c
@@ -5,8 +5,43 @@
 #include "Utils/Battery.h"
 #include "Utils/Calculations.h"
 #include "Utils/Pyro.h"
+#include "KalmanLib.h"
 
 #define MM_TO_METERS 0.001f
+#define KALMAN_DT 0.01f
+
+static arm_matrix_instance_f32 kalman_P, kalman_Q, kalman_R_GPS, kalman_R_MAG, kalman_R_BAR;
+static float32_t kalman_P_data[H_P_A_Q_COLS * H_P_A_Q_COLS];
+static float32_t kalman_Q_data[H_P_A_Q_COLS * H_P_A_Q_COLS];
+static float32_t kalman_R_GPS_data[H_GPS_ROWS * H_GPS_ROWS] = {0};
+static float32_t kalman_R_MAG_data[H_MAG_ROWS * H_MAG_ROWS];
+static float32_t kalman_R_BAR_data[H_BAR_ROWS * H_BAR_ROWS];
+
+static float32_t kalman_pos[3] = {0};
+static float32_t kalman_vel[3] = {0};
+static float32_t kalman_quat[4] = {0};
+
+static float32_t kalman_B_e[3] = {0};
+static float32_t kalman_z_GPS[6] = {0};
+static float32_t kalman_z_MAG[3] = {0};
+
+static bool kalman_initialized = false;
+static float kalman_last_pressure = 0.0f;
+
+void KalmanFilter_Init(SystemContext_t *SystemContext) {
+	float32_t eul_deg_0[3] = {0.0f, 90.0f, 0.0f};
+
+	for (int i = 0; i < H_GPS_ROWS; i++) {
+		kalman_R_GPS_data[i + i * H_GPS_ROWS] = 1.0f;
+	}
+	arm_mat_init_f32(&kalman_R_GPS, H_GPS_ROWS, H_GPS_ROWS, kalman_R_GPS_data);
+
+	kalman_init(&kalman_P, &kalman_Q, &kalman_R_GPS, &kalman_R_MAG, &kalman_R_BAR,
+		&kalman_P_data, &kalman_Q_data, &kalman_R_MAG_data, &kalman_R_BAR_data,
+		&eul_deg_0, &kalman_quat, KALMAN_DT);
+
+	kalman_initialized = true;
+}
 
 FlightData_t GetFlightData(SystemState_t SystemState, SystemContext_t *SystemContext, IIM42653_SensorData_t IIM42653_FlightData, BMP581_SensorData_t BMP581_FlightData, IIS2MDCTR_SensorData_t IIS2MDCTR_FlightData, ZOEM8Q_SensorData_t ZOEM8Q_FlightData, CommandType_t LastCommand) {
 	FlightData_t FlightData;
@@ -45,9 +80,28 @@ FlightData_t GetFlightData(SystemState_t SystemState, SystemContext_t *SystemCon
 	FlightData.BarometricVelocity = CalculateBarometricVerticalVelocity(FlightData.BarometricAltitude, FlightData.Tick);
 	FlightData.GPSVelocity = CalculateGPSVerticalVelocity(FlightData.GPSAltitude, FlightData.Tick);
 
-	FlightData.VelX = 0;
-	FlightData.VelY = 0;
-	FlightData.VelZ = 0;
+	if (kalman_initialized) {
+		bool BAR_available = (FlightData.PressurePa != kalman_last_pressure);
+		kalman_last_pressure = FlightData.PressurePa;
+
+		float32_t accel_IMU[3] = {FlightData.AccelX, FlightData.AccelY, FlightData.AccelZ};
+		float32_t omega_IMU[3] = {FlightData.GyroX, FlightData.GyroY, FlightData.GyroZ};
+		float32_t z_BAR[1] = {FlightData.PressurePa};
+
+		kalman_filter(true, false, false, BAR_available,
+			&accel_IMU, &omega_IMU, &kalman_z_GPS, &kalman_z_MAG, &z_BAR,
+			&kalman_pos, &kalman_vel, &kalman_quat,
+			&kalman_B_e, &kalman_P, &kalman_Q, &kalman_R_GPS, &kalman_R_MAG, &kalman_R_BAR,
+			SystemContext->ReferencePressurePa, KALMAN_DT);
+
+		FlightData.VelX = kalman_vel[0];
+		FlightData.VelY = kalman_vel[1];
+		FlightData.VelZ = kalman_vel[2];
+	} else {
+		FlightData.VelX = 0;
+		FlightData.VelY = 0;
+		FlightData.VelZ = 0;
+	}
 
 	FlightData.Flags = SystemFaultFlags;
 	FlightData.BatteryVoltage = BatteryGetVoltage();
