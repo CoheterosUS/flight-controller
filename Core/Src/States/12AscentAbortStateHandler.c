d798b873e2fa478d80d796840e60de424eb963db
Angelo Sho Moraschi <68395930+amoraschi@users.noreply.github.com>
2026-09-23 10:34:19 +0200

    feat: IMU calibration script

--- Core/Src/States/12AscentAbortStateHandler.c
+++
@@ -1,9 +0,0 @@
-#include "States/StateHandlers.h"
-#include "Utils/Pyro.h"
-
-void AscentAbortStateEntry(SystemContext_t *Context) {
-}
-
-SystemState_t AscentAbortStateHandler(SystemContext_t *Context, FlightData_t FlightData) {
-    return STATE_ASCENT_ABORT;
-}
